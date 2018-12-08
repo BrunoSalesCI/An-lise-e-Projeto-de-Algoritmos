89b75c9082d9ca9d6b3750d7c28095808a5a9933
Bruno Sales <30627720+BrunoSalesCI@users.noreply.github.com>
2018-12-08 13:04:41 -0200

    Update insertionSort.c

--- insertionSort.c
+++ insertionSort.c
@@ -11,10 +11,10 @@ void InsertionSort (int list[], int elements){
 		y = i-1;
 
 		while(y >= 0 && list[y] > x){			//Se o número correspondente ao indice
-			list[y+1] = list[y];				//de Y for maior que o do índice X, executa o while: [o valor Y vai para 
-			y = y-1;							//valor do proximo indice, no caso o X]
+			list[y+1] = list[y];			//de Y for maior que o do índice X, executa o while: [o valor Y vai para 
+			y = y-1;				//valor do proximo indice, no caso o X]
 		}
-		list[y+1] = x;							//A posição inicial do Y agora recebe o valor de X
+		list[y+1] = x;					//A posição inicial do Y agora recebe o valor de X
 	}
 	for(n=0; n < elements; n++)
 		printf("#%d -> %d \n", n, list[n]);

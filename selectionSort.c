3ad2c302cb0b022262d2dbd1c359a92a1d130704
Bruno Sales <30627720+BrunoSalesCI@users.noreply.github.com>
2018-12-08 13:05:36 -0200

    Update selectionSort.c

--- selectionSort.c
+++ selectionSort.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 #include <math.h>
 
-void selectionSort(int list[], int elements){ //recebe um array e o número de elementos
+void selectionSort(int list[], int elements){ 		//recebe um array e o número de elementos
 	int i, j, aux, min, n;
 
-	for(i=0; i<elements-1; i++){ //Percorre todo o array
+	for(i=0; i<elements-1; i++){ 			//Percorre todo o array
 
-		min = i; //Guarda o valor do menor índice para min
+		min = i; 				//Guarda o valor do menor índice para min
 		
-		for(j=i+1; j<elements; j++){ //percorre o resto do array, comparando com o i
-			if(list[j] < list[min]) //compara o valor de j x min, caso j for menor, j é o novo min
+		for(j=i+1; j<elements; j++){		 //percorre o resto do array, comparando com o i
+			if(list[j] < list[min])		 //compara o valor de j x min, caso j for menor, j é o novo min
 				min = j;
 				
 				aux = list[i];		
-				list[i] = list[min]; //atribui-se ao elemento de índice 'i' o valor mínimo
-				list[min] = aux; //a variável em questão é designada ao elemento mínimo, para fazer outra comparação
+				list[i] = list[min];	 //atribui-se ao elemento de índice 'i' o valor mínimo
+				list[min] = aux; 	//a variável em questão é designada ao elemento mínimo, para fazer outra comparação
 		}
 	}
 	for(n=0; n < elements; n++)

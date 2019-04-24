920a8086050824d2e6389f10126db12331a9b636
Bruno Sales <30627720+BrunoSalesCI@users.noreply.github.com>
2019-04-23 23:12:19 -0300

    Adicionado somente comentários

--- mochilaInteira/mochilaInteira.py
+++ mochilaInteira/mochilaInteira.py
@@ -3,37 +3,41 @@ import timeit
 import numpy as np
 
 if len(sys.argv) != 2:
-    arq = open("instancias/mochila01.txt.txt","r")	#Lê o arquivo
+    arq = open("instancias/mochila01.txt.txt","r")				#Leitura do arquivo
 else:
     arq = open("instancias/"+sys.argv[1],"r")
-mat_nx2 = []
+matriz = []
 for j in arq:
-	mat_nx2.append(map(int,j.split()))
+	matriz.append(map(int,j.split()))
 
-def Init_mat(lines,columns):						#Inicia uma matriz de tamanho linhas x colunas, com zeros
-	mat = np.zeros((lines,columns),dtype=int)
-	return mat
 
-def MaxMochila(mat_nx2):
-    mat = Init_mat(mat_nx2[0][0]+1,mat_nx2[0][1]+1) # Inicia a Matriz (nxM - Objetos x Capacidade) com 0
-    for j in range(1,mat_nx2[0][0]+1):
-        for w in range(1,mat_nx2[0][1]+1):
-            if(mat_nx2[j][0] > w):  				# Verifica se o peso do item atual é maior que a capacidade da mochila
-                mat[j][w] = mat[j-1][w] 
+
+
+def Init_mat(lines,columns):							#Inicia uma matriz de tamanho linhas x colunas, com zeros
+	mat = np.zeros((lines,columns),dtype=int)				#Seta as linhas e colunas com zeros, tipo = int
+	return mat   								#Retorna a matriz 'mat'
+
+def MaxMochila(matriz):
+    mat = Init_mat(matriz[0][0]+1,matriz[0][1]+1)  				#Inicia a Matriz (nxM - Objetos x Capacidade) com 0
+    for j in range(1,matriz[0][0]+1):						#Começa a percorrer a matriz
+        for w in range(1,matriz[0][1]+1):					#w é a capacidade da mochila / j é o peso do item
+            if(matriz[j][0] > w):  						#Verifica se o peso do item atual é maior que a capacidade da mochila
+                mat[j][w] = mat[j-1][w] 					#Se entrar nessa condição, objeto não cabe na mochila. Então, põe o peso do item anterior
             else:
-                col = [w-mat_nx2[j][0] if w-mat_nx2[j][0]>0 else 0]
-                mat[j][w] = max(mat[j-1][w] , mat[j-1][col] + mat_nx2[j][1])
-    return(mat)
+                col = [w-matriz[j][0] if w-matriz[j][0]>0 else 0]		#Coloca o item na mochila, subtraindo o seu peso pela capacidade da mochila
+                mat[j][w] = max(mat[j-1][w] , mat[j-1][col] + matriz[j][1])
+    return(mat)									#Retorna o valor dos itens na mochila
 
-def GetMochila(mat,mat_nx2):
-    col = len(mat[0])-1
-    elements = []
-    for row in range(len(mat)-1,-1,-1):
-        if(mat[row][col] != mat[row-1][col]):
-            elements.append(row)
-            col = abs(col-mat_nx2[row][0])
-    return(elements)
 
-mat = MaxMochila(mat_nx2)
+def GetMochila(mat,matriz):					#Função que percorre a matriz para ver os elementos que estão nela
+    col = len(mat[0])-1						#Seta col como o tamanho da coluna da matriz mat[]
+    elements = []						#Cria um array 'elements' para armazenar os elementos da matriz
+    for row in range(len(mat)-1,-1,-1):				#Laço de percorrimento da matriz
+        if(mat[row][col] != mat[row-1][col]):			#Se o elemento da linha[] e coluna[] for diferente da linha anterior significa que é um novo item dentro da mochila
+            elements.append(row)				#Adiciona esse item no array 'elements'
+            col = abs(col-matriz[row][0])			#Retorna o valor absoluto dessa func para 'col'
+    return(elements)						#Retorna 'elements' com os valores dos elementos que estão na mochila (na matriz mat[])
 
-print("Valor Arrecadado na Mochila:{}\nItens Escolhidos:{}".format(mat[-1][-1],GetMochila(mat,mat_nx2))) #Printa mat
+mat = MaxMochila(matriz)					#mat = valor dos itens da mochila
+
+print("Valor Arrecadado na Mochila:{}\nItens Escolhidos:{}".format(mat[-1][-1],GetMochila(mat,matriz))) #Printa a saída
